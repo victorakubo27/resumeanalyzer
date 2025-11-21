55627d9634076f422e55feb55a24153bef4c619c
victorakubo27 <victorakubo27@gmail.com>
2025-11-21 05:23:46 +0000

    auth theme setting

--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -15,10 +15,10 @@ const Auth = () => {
     return (
         <main className="bg-[url('/images/bg-auth.svg')] dark:bg-[url('/images/bg-black-main.svg')] bg-cover min-h-screen flex items-center justify-center">
             <div className="gradient-border shadow-lg">
-                <section className="flex flex-col gap-8 bg-white rounded-2xl p-10">
+                <section className="flex flex-col gap-8 bg-white dark:bg-neutral-700 rounded-2xl p-10">
                     <div className="flex flex-col items-center gap-2 text-center">
                         <h1>Welcome</h1>
-                        <h2>Log In to Continue Your Job Journey</h2>
+                        <h2 className="dark:!text-gray-200">Log In to Continue Your Job Journey</h2>
                     </div>
                     <div>
                         {isLoading ? (
